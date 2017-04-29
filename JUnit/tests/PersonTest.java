150e08d4a9d7894adf8a587cc14e8243d13191b1
Lena900925 <rinyu.annemarie.hd@gmail.com>
2017-04-29 19:53:41 +0200

    Added a new project

---
+++ JUnit/tests/PersonTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Created by arinyu on 2017.04.29..
+ */
+class PersonTest {
+    @Test
+    public void testIsNameIsNotNull() {
+        Person john = new Person("John", 30);
+        assertNotNull(john.getName());
+    }
+    @Test
+    public void testIsAgeBelow0ThrowsException(){
+        Person john = new Person();
+        assertThrows(IllegalArgumentException.class, () -> {john.setAge(-10);
+        });
+    }
+}
